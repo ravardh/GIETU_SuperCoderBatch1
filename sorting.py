476419cb07f7f0deefb67792d9b441c9f9eea628
AArdhendu <119116901+AArdhendu@users.noreply.github.com>
2024-02-22 11:13:12 +0530

    Add files via upload

---
+++ sorting.py
@@ -0,0 +1,35 @@
+#bubble_sort
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return(arr)
+print(bubble_sort([30,23,10,20]))
+
+#selection_sort
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return(arr)
+print(selection_sort([30,26,10,20]))
+
+#insertion_sort
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+arr = [4, 7, 8, 1, 9, 2, 3, 6, 5]
+insertion_sort(arr)
+print("Insertion Sort:", arr)
