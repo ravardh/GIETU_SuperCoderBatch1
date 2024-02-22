999c03a913ea7684f2f975a9c8f035b487f2837f
prahalladamahapatro <94302612+prahalladamahapatro@users.noreply.github.com>
2024-02-22 11:22:34 +0530

    Add files via upload

---
+++ selectionsort.py
@@ -0,0 +1,16 @@
+#In Selection Sort, the smallest element is sorted first with the 'i'th value and the process is continuous.
+
+l = [4,7,8,1,9,2,3,6,5]
+
+def selectionSort(l):
+    n = len(l)
+    for i in range(n):
+        minpos = i
+        for j in range(i+1,n):
+            if l[j] < l[minpos]:
+                minpos = j
+
+        l[i],l[minpos] = l[minpos],l[i]
+    return l
+
+print(selectionSort(l))
