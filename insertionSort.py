821a8f42485d2ca996a919cd13ac25da4995a8a1
Smruti Sundar pradhan <152421445+Pupun2023@users.noreply.github.com>
2024-02-22 10:54:46 +0530

    insertionSort

---
+++ insertionSort.py
@@ -0,0 +1,12 @@
+l = [4,7,8,1,9,2,3,6,5]
+
+def insertionSort(l):
+    n = len(l)
+    for i in range(n):
+        minpos = i
+        while minpos > 0 and l[minpos]<l[minpos-1]:
+            l[minpos],l[minpos-1] = l[minpos-1],l[minpos]
+            minpos -= 1
+    return l
+
+print(insertionSort(l))
