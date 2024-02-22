938195ad095c82b07a15f3e86ffae493bce2f475
NaiduSudikhya <150261368+NaiduSudikhya@users.noreply.github.com>
2024-02-22 11:27:15 +0530

    Add files via upload

---
+++ Insertionsort.py
@@ -0,0 +1,21 @@
+n=int(input("Enter the number of elements:"))
+list1=[]
+for i in range(n):
+    m=int(input())
+    list1.append(m)
+print(list1)
+
+def insertion_sort(list1):
+
+        for i in range(1, len(list1)):
+            a = list1[i]
+            j = i - 1
+            while j >= 0 and a < list1[j]:
+                list1[j + 1] = list1[j]
+                j -= 1
+            list1[j + 1] = a
+        return list1
+
+
+print("The unsorted list is:", list1)
+print("The sorted new list is:", insertion_sort(list1))
