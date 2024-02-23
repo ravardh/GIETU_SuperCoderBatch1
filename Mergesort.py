900e0a4294952aa4cef554a624903bc977cbad95
Arup7733 <138898081+Arup7733@users.noreply.github.com>
2024-02-23 09:59:08 +0530

    Add files via upload

---
+++ Mergesort.py
@@ -0,0 +1,32 @@
+def mergeSort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left_half = arr[:mid]
+    right_half = arr[mid:]
+
+    return merge( mergeSort(left_half),  mergeSort(right_half))
+
+def merge(left, right):
+    merged = []
+    left_index = 0
+    right_index = 0
+
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] <= right[right_index]:
+            merged.append(left[left_index])
+            left_index += 1
+        else:
+            merged.append(right[right_index])
+            right_index += 1
+
+    merged.extend(left[left_index:])
+    merged.extend(right[right_index:])
+
+    return merged
+
+arr = [4, 8, 2, 3, 7, 5, 1, 9, 6]
+sorted_numbers = mergeSort(arr)
+
+print(sorted_numbers)
