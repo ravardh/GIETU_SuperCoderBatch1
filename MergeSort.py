80f13bda175e5433284fd60f394d776444741852
Krushna S. Baral <117432295+krishnasbaral@users.noreply.github.com>
2024-02-23 11:13:26 +0530

    Add files via upload

---
+++ MergeSort.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 23 10:44:10 2024
+
+@author: HP
+"""
+
+def mergeSort(A):
+    if len(A) < 2:
+        return A
+    mid = int(len(A)/2)
+    left = mergeSort(A[:mid])
+    right = mergeSort(A[mid:])
+
+    r,l = 0,0
+    B = []
+
+    while len(B) < len(A):
+        if r >= len(right) or (l < mid and left[l] < right[r]):
+            B.append(left[l])
+            l += 1
+        elif r < len(right):
+            B.append(right[r])
+            r += 1
+
+    return B
+A=eval(input("Enter a list of elements for sorting: "))
+print(mergeSort(A))
