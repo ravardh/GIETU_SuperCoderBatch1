b6594b10bb0b64c108adf1871cff9c9e2871ca0d
rachanapanda7 <146992355+rachanapanda7@users.noreply.github.com>
2024-02-23 10:00:54 +0530

    Add files via upload

---
+++ bubble_sorting.py
@@ -0,0 +1,16 @@
+n=int(input("Enter the number:"))
+list_1 = []
+for i in range(n):
+    m=int(input())
+    list_1.append(m)
+print(list_1)
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+                
+bubble_sort(list_1)
+print("Sorted array:", list_1)
