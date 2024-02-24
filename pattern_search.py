d2e6278a2e575d1dbf18115e092875c9659a6d5e
Smruti Sundar pradhan <152421445+Pupun2023@users.noreply.github.com>
2024-02-24 10:27:28 +0530

    Add files via upload

---
+++ pattern_search.py
@@ -0,0 +1,14 @@
+#string =ABABCABACBCABCABABCABACB
+#pattern=ABC
+#string ="ABABCABACBCABCABABCABACB"
+string=input("Enter the string:\n")
+pattern=input("Enter the pattern:\n")
+#pattern="ABC"
+flag=False
+len_pattern=len(pattern)
+for i in range(len(string)):
+    if(string[i:i+len_pattern]==pattern):
+        print("found at:",i,"index")
+        flag=True
+if(flag==False):
+    print("Pattern not found")
