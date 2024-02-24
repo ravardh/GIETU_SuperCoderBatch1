8552f64c45eb373467c84a4121b78231a4235903
OmmprakashMohanty01 <160700580+OmmprakashMohanty01@users.noreply.github.com>
2024-02-24 10:15:47 +0530

    Add files via upload

---
+++ list tnp.py
@@ -0,0 +1,10 @@
+#Dictionary 
+# key and value pairs 
+s={1:"aishwarya",2:"saswitha"}
+print(s)
+#update - add new element with key
+s.update({3:"ruchi"})
+print(s)
+#update old element with old key 
+s.update({1:"aishwarya"})
+print(s)
