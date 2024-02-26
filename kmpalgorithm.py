9fab886a02eb895f3bcf55e1fd7b8a979a477381
prahalladamahapatro <94302612+prahalladamahapatro@users.noreply.github.com>
2024-02-26 12:39:34 +0530

    Add files via upload

---
+++ kmpalgorithm.py
@@ -0,0 +1,42 @@
+def KMPStringMatching(pat, st):
+    M = len(pat)
+    N = len(st)
+    lps = [0]*M
+    j = 0 
+    computeLPSArray(pat, M, lps)
+    i = 0
+    
+    while i < N:
+        if pat[j] == st[i]:
+            i += 1
+            j += 1
+  
+        if j == M:
+            print ("Found pattern at index " + str(i-j))
+            j = lps[j-1]
+  
+        elif i < N and pat[j] != st[i]:          
+            if j != 0:
+                j = lps[j-1]
+            else:
+                i += 1
+  
+def computeLPSArray(pat, M, lps):
+    len = 0 
+    lps[0] 
+    i = 1
+    
+    while i < M:
+        if pat[i]== pat[len]:
+            len += 1
+            lps[i] = len
+            i += 1
+
+        else:   
+            if len != 0:
+                len = lps[len-1]
+            else:
+                lps[i] = 0
+                i += 1
+
+KMPStringMatching('ababcabcabababd','abab')
