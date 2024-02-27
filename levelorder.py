23d45d75bb0faf1369435bf5c1cf74487e6b9fea
NaiduSudikhya <150261368+NaiduSudikhya@users.noreply.github.com>
2024-02-27 08:59:41 +0530

    Add files via upload

---
+++ levelorder.py
@@ -0,0 +1,36 @@
+class Node:
+    def __init__(self,info):
+        self.data=info
+        self.left=None
+        self.right=None
+root=Node(20)
+root.left=Node(32)
+root.right=Node(10)
+root.left.left=Node(15)
+root.left.right=Node(12)
+root.right.left=Node(8)
+root.right.right=Node(24)
+root.left.left.left=Node(4)
+root.left.right.left=Node(50)
+root.left.right.right=Node(6)
+root.left.right.left.left=Node(40)
+root.left.right.left.right=Node(35)
+root.right.right.left=Node(16)
+root.right.right.right=Node(2)
+def levelorder(root):
+    q = [root]
+    q.append(None)
+    while len(q) > 0:
+        cur = q.pop(0)
+        if cur == None:
+            print()
+            if len(q) > 0:
+                q.append(None)
+        else:
+            print(cur.data, end=' ')
+            if cur.left != None:
+                q.append(cur.left)
+            if cur.right != None:
+                q.append(cur.right)
+
+levelorder(root)
