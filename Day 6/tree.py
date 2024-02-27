bbbbe77eefc305ab4fe30d70cd6347c7924d2017
Ankit Choudhury <ankit.choudhury0608@gmail.com>
2024-02-27 08:49:02 +0530

    Day 6
    Tree Traversal

---
+++ Day 6/tree.py
@@ -0,0 +1,83 @@
+class Node:
+  def __init__(self, data=None) -> None:
+    self.data = data
+    self.left = None
+    self.right = None
+
+def preorder(root: Node) -> None: # Root, Left, Right
+  if root == None:
+    return
+  
+  print(root.data, end=" --> ")
+  preorder(root.left)
+  preorder(root.right)
+
+def inorder(root: Node) -> None: # Left, Root, Right
+  if root == None:
+    return
+  
+  inorder(root.left)
+  print(root.data, end=" --> ")
+  inorder(root.right)
+
+def postorder(root: Node) -> None: # Left, Right, Root
+  if root == None:
+    return
+  
+  postorder(root.left)
+  postorder(root.right)
+  print(root.data, end=" --> ")
+
+''' Level Order Algo [BFS]
+Use Queue DS
+Initially add root and None
+While removing any node add all it's child
+While removing None Print new line and add None again
+'''
+def levelorder(root):
+  q=[root]
+  q.append(None)
+  i=1
+  print(f"\nLevel {i}")
+  while len(q)>0:
+    curr = q.pop(0)
+    if curr == None:
+      i += 1
+      if len(q) > 0:
+        print(f"\nLevel {i}")
+        q.append(None)
+    else:
+      print(curr.data, end="\t")
+      if curr.left != None:
+        q.append(curr.left)
+      if curr.right != None:
+        q.append(curr.right)
+
+root = Node(20)
+
+root.left = Node(32)
+root.left.left = Node(15)
+root.left.right = Node(12)
+root.left.left.left = Node(4)
+root.left.right.left = Node(50)
+root.left.right.right = Node(6)
+root.left.right.left.left = Node(40)
+root.left.right.left.right = Node(35)
+
+root.right = Node(10)
+root.right.left = Node(8)
+root.right.right = Node(24)
+root.right.right.left = Node(16)
+root.right.right.right = Node(2)
+
+print("Pre Order [Root, Left, Right]")
+preorder(root)
+
+print("\nIn Order [Left, Root, Right]")
+inorder(root)
+
+print("\nPost Order [Left, Right, Root]")
+postorder(root)
+
+print("\nLevel Order [BFS]")
+levelorder(root)
--- README.md
+++ README.md
@@ -4,4 +4,5 @@
 | Day 2      | Match case, calculator | 1                      | --- | [Link](./Day%202/)  |
 | Day 3      | Datatypes, Sorting Algorithms | 2               | Buuble, Selection, Insertion Sort | [Link](./Day%203/) |
 | Day 4      | Max_k_consecutive_sum, Sorting Algorithms | 2   | Sliding window, Quick and Merge Sort | [Link](./Day%204/) |
-| Day 5      | String matching | 1                | KMP Algo       | [Link](./Day%205/)  |
+| Day 5      | String matching, Linked List | 2                | KMP Algo       | [Link](./Day%205/)  |
+| Day 6      | Tree, Traversal | 1                    |  | [Link](./Day%206/) |
