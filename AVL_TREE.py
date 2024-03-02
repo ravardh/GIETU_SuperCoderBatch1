641a81265165923f0b762ce60c718e6c7add5de7
subham Mallick <136732776+subhammallick4444@users.noreply.github.com>
2024-03-02 09:26:43 +0530

    Update AVL_TREE.py

--- AVL_TREE.py
+++ AVL_TREE.py
@@ -10,63 +10,65 @@ class AVLTree(object):
     def insert(self, root, value):
         if root is None:
             return node(value)
-
         if value < root.data:
             root.left = self.insert(root.left, value)
         elif value > root.data:
             root.right = self.insert(root.right, value)
         
-        root.height = 1 + max(self.height(root.left), self.height(root.right))
+        root.height = 1 + max(self.ght(root.left),
+                              self.ght(root.right))
 
-        balance = self.balance(root)
 
-        if balance > 1 and value < root.left.data:
-            return self.rightrotate(root)
+        bl = self.bal(root)
 
-        if balance < -1 and value > root.right.data:
+        if bl > 1 and value < root.left.data:
             return self.leftrotate(root)
 
-        if balance > 1 and value > root.left.data:
-            root.left = self.leftrotate(root.left)
-            return self.rightrotate(root)
-
-        if balance < -1 and value < root.right.data:
-            root.right = self.rightrotate(root.right)
+        if bl > 1 and value > root.left.data:
+            root.left = self.rightrotate(root.left)
             return self.leftrotate(root)
 
+        if bl < -1 and value > root.right.data:
+            return self.rightrotate(root)
+
+        if bl < -1 and value < root.right.data:
+            root.right = self.leftrotate(root.right)
+            return self.rightrotate(root)
         return root
 
-    def height(self, root):
+    def ght(self, root):
         if root is None:
             return 0
         return root.height
 
-    def balance(self, root):
+    def bal(self, root):
         if root is None:
             return 0
-        return self.height(root.left) - self.height(root.right)
+        return self.ght(root.left) - self.ght(root.right)
 
     def leftrotate(self, A):
-        B = A.right
-        T = B.left
+        if A is None or A.left is None:
+            return A
+        B = A.left
+        temp = B.right
+        B.right = A
+        A.left = temp
 
-        B.left = A
-        A.right = T
-
-        A.height = 1 + max(self.height(A.left), self.height(A.right))
-        B.height = 1 + max(self.height(B.left), self.height(B.right))
-
+        A.height = 1 + max(self.ght(A.left), self.ght(A.right))
+        B.height = 1 + max(self.ght(B.left), self.ght(B.right))
         return B
 
     def rightrotate(self, A):
-        B = A.left
-        T = B.right
+        if A is None or A.right is None:
+            return A
 
-        B.right = A
-        A.left = T
+        B = A.right
+        temp = B.left
+        B.left = A
+        A.right = temp
 
-        A.height = 1 + max(self.height(A.left), self.height(A.right))
-        B.height = 1 + max(self.height(B.left), self.height(B.right))
+        A.height = 1 + max(self.ght(A.left), self.ght(A.right))
+        B.height = 1 + max(self.ght(B.left), self.ght(B.right))
 
         return B
 
@@ -81,9 +83,10 @@ class AVLTree(object):
 tree = AVLTree()
 
 root = None
-V_L = [50, 36, 12, 20, 76, 18, 44, 52, 90]
+V_L = [50, 45, 55, 35, 40, 48, 60, 20, 70, 41, 47, 42, 15, 22, 25, 30, 90]
 for x in V_L:
     root = tree.insert(root, x)
 
-print(root.data)  # Output: 36
-tree.inorder(root)  # Output: 12 18 20 36 44 50 52 76 90
+print(root)
+
+tree.inorder(root)
