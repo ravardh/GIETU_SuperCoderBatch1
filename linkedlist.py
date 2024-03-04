ce68058e33887abbf761c57cf5cbc9f3a382bed0
AnkitaDas421 <143398007+AnkitaDas421@users.noreply.github.com>
2024-03-04 18:03:17 +0530

    Add files via upload

---
+++ linkedlist.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end=" ")
+            current = current.next
+        print()
+
+my_linked_list = LinkedList()
+my_linked_list.append(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+
+print("Linked List Elements:")
+my_linked_list.display()
+
