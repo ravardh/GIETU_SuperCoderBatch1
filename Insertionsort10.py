40069be080b5693caf9630fa4fe2efb9c0999aa5
Sritendu26 <160699198+Sritendu26@users.noreply.github.com>
2024-03-04 10:00:54 +0530

    Add files via upload

---
+++ Insertionsort10.py
@@ -0,0 +1,14 @@
+def insertionSort(arr):
+    n = len(arr)
+    for i in range(1, n):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+# Example usage
+arr = eval(input("Enter a list of elements for sorting (separated by spaces): "))
+insertionSort(arr)
+print("After Sorting:", arr)
