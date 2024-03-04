62d6230f92edb49a44db3225ef390f76a0e40587
Biswajit Sahoo <160457169+Hypercoder09@users.noreply.github.com>
2024-03-04 09:10:26 +0530

    Add files via upload

---
+++ tree.py
@@ -0,0 +1,129 @@
+'''class node:
+    def __init__ (self,data=None):
+        self.data=data
+        self.left=None
+        self.right=None
+
+def preorder(root):
+    if(root==None):
+        return
+    print(root.data)
+    preorder(root.left)
+    preorder(root.right)
+
+
+
+def inorder(root):
+    if(root==None):
+        return
+    preorder(root.left)
+    print(root.data)
+    preorder(root.right)
+
+
+def postorder(root):
+    if(root==None):
+        return
+    
+    preorder(root.left)
+    preorder(root.right)
+    print(root.data)
+    
+root=node(20)
+root.left=node(10)
+root.right=node(12)
+
+root.left.left=node(30)
+root.left.right=node(45)
+root.left.left.left=node(4)
+root.left.left.right=node(5)
+
+
+print("preorder...")
+preorder(root)
+
+print("inorder...")
+inorder(root)
+
+print("postorder...")
+postorder(root) '''
+
+
+class node:
+    def _init_(self,info=None):
+        self.data = info
+        self.left = None
+        self.right = None
+
+
+def preorder(root): #root, left, right
+    if root==None: #Base case
+        return
+    print(root.data, end=" ") #Executabale Statement to print Root data
+    preorder(root.left) #recursive Call for left child
+    preorder(root.right) #recursive Call for right child
+    
+
+def inorder(root): #root, left, right
+    if root==None: #Base case
+        return
+    inorder(root.left) #recursive Call for left child
+    print(root.data, end=" ") #Executabale Statement to print Root data
+    inorder(root.right) #recursive Call for right child
+
+def postorder(root): #root, left, right
+    if root==None: #Base case
+        return
+    postorder(root.left) #recursive Call for left child
+    postorder(root.right) #recursive Call for right chfild
+    print(root.data, end=" ") #Executabale Statement to print Root data
+    
+def levelorder(root):
+    Q=[root]
+    Q.append(None)
+    while len(Q)>0:
+        cur = Q.pop(0)
+        if cur == None:
+            print()
+            if len(Q)>0:
+                Q.append(None)
+        else:
+            print(cur.data, end = " ")
+            if cur.left != None:
+                Q.append(cur.left)
+            if cur.right != None:
+                Q.append(cur.right)
+            
+         
+
+root = node(20)
+
+root.left = node(32)
+root.right = node(10)
+
+root.left.left = node(15)
+root.left.right = node(12)
+root.right.left = node(8)
+root.right.right = node(24)
+
+root.left.left.left = node(4)
+root.left.right.left = node(50)
+root.left.right.right = node(6)
+root.right.right.left = node(16)
+root.right.right.right = node(2)
+
+root.left.right.left.left = node(40)
+root.left.right.left.right = node(35)
+
+
+print(root)
+print(root.data)
+print("\nPreorder Traversal")
+preorder(root)
+print("\n\nInorder Traversal")
+inorder(root)
+print("\n\nPostorder Traversal")
+postorder(root)
+
+print("\n\nLevelorder Traversal")
+levelorder(root)
