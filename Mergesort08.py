20a5c9cbba162fa1fd5e6fec964e86f4397ff022
Sritendu26 <160699198+Sritendu26@users.noreply.github.com>
2024-03-04 09:44:17 +0530

    Add files via upload

---
+++ Mergesort08.py
@@ -0,0 +1,32 @@
+def mergeSort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        L, R = arr[:mid], arr[mid:]
+        mergeSort(L)
+        mergeSort(R)
+
+        i, j, k = 0, 0, 0
+        while i < len(L) and j < len(R):
+            if L[i] <= R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+
+# Example usage
+arr = [12, 11, 13, 5, 6, 7]
+mergeSort(arr)
+
+print("Sorted array is:", arr)
