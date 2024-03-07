cf6433553a596d40b170976ed8482c39482dd347
Biswajit Sahoo <160457169+Hypercoder09@users.noreply.github.com>
2024-03-07 09:06:26 +0530

    Add files via upload

---
+++ Graph(BFS).py
@@ -0,0 +1,43 @@
+class Graph:
+    def __init__(self,v):
+        self.v = v
+        self.adj_list = [[] for _ in range(v)]
+        self.visited = [] 
+
+    def add_edge(self,s,d,w):
+        self.adj_list[s-1].append((s,d,w))
+        
+    def bfs(self, start):
+        visited = set()
+        queue = [start]
+        while queue:
+            node = queue.pop(0)
+            if node not in visited:
+                print(node, end=" ")
+                visited.add(node)
+                for i in range(len(self.adj_list[node-1])):
+                    x=self.adj_list[node-1][i][1]
+                    if x not in visited:
+                        queue.append(x)
+       
+graph = Graph(9)
+graph.add_edge(1,2,1)
+graph.add_edge(1,3,1)
+graph.add_edge(1,9,1)
+graph.add_edge(2,4,1)
+graph.add_edge(2,1,1)
+graph.add_edge(3,1,1)
+graph.add_edge(3,8,1)
+graph.add_edge(4,5,1)
+graph.add_edge(4,6,1)
+graph.add_edge(4,2,1)
+graph.add_edge(5,4,1)
+graph.add_edge(6,4,1)
+graph.add_edge(6,7,1)
+graph.add_edge(7,6,1)
+graph.add_edge(7,8,1)
+graph.add_edge(8,3,1)
+graph.add_edge(8,9,1)
+graph.add_edge(9,1,1)
+graph.add_edge(9,8,1)
+graph.bfs(9)
