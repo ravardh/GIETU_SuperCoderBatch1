2710d88db07ff2d430a53178c26b264ba68a4966
Jyoti Ranjan Barik <104340440+Jyotiranjan2@users.noreply.github.com>
2024-03-16 09:31:42 +0530

    Add files via upload

---
+++ merge.py
@@ -0,0 +1,38 @@
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left_half = arr[:mid]
+    right_half = arr[mid:]
+
+    left_half = merge_sort(left_half)
+    right_half = merge_sort(right_half)
+
+    return merge(left_half, right_half)
+
+def merge(left, right):
+    merged = []
+    left_index = right_index = 0
+
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] < right[right_index]:
+            merged.append(left[left_index])
+            left_index += 1
+        else:
+            merged.append(right[right_index])
+            right_index += 1
+
+    merged.extend(left[left_index:])
+    merged.extend(right[right_index:])
+
+    return merged
+
+arr = input("Enter a list of numbers separated by spaces: ").split()
+arr = [int(x) for x in arr]
+
+
+sorted_arr = merge_sort(arr)
+
+
+print("Sorted array:", sorted_arr)
