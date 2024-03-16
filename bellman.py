45238182a6bc11d2cbe41f6c388fdb20ef453aeb
sradhanjali panda <138843797+sradha8480@users.noreply.github.com>
2024-03-16 08:47:24 +0530

    Add files via upload

---
+++ bellman.py
@@ -0,0 +1,31 @@
+class Graph:
+    def _init_(self,v):
+        self.v = v
+        self.adj_list = [[] for _ in range(v)]
+        self.dict={}
+        for i in range(1,v+1):
+            self.dict[i]=float('inf')
+        
+    def add_edge(self,s,d,w):
+        self.adj_list[s-1].append((s,d,w))
+        
+    def bellman_ford(self,source):
+        self.dict[source]=0
+        for i in range(self.v-1):
+            for s,d,w in self.adj_list[i]:
+                if self.dict[s]!=float('inf') and self.dict[d]>self.dict[s]+w:
+                    self.dict[d]=self.dict[s] + w
+        print(self.dict)
+               
+graph = Graph(7)
+graph.add_edge(1,2,6)
+graph.add_edge(1,3,5)
+graph.add_edge(1,4,5)
+graph.add_edge(2,5,-1)
+graph.add_edge(3,2,-2)
+graph.add_edge(3,5,1)
+graph.add_edge(4,3,-2)
+graph.add_edge(4,6,-1)
+graph.add_edge(5,7,3)
+graph.add_edge(6,7,3)
+graph.bellman_ford(1)
