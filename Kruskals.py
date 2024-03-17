580bc8b7417882db7e16b371b328e61ca9ef0f3f
Arup7733 <138898081+Arup7733@users.noreply.github.com>
2024-03-17 23:45:50 +0530

    Add files via upload

---
+++ Kruskals.py
@@ -0,0 +1,54 @@
+class Graph:
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = []
+
+    def add_edge(self, u, v, w):
+        self.graph.append([u, v, w])
+
+    def find(self, parent, i):
+        if parent[i] == i:
+            return i
+        return self.find(parent, parent[i])
+
+    def apply_union(self, parent, rank, x, y):
+        xroot = self.find(parent, x)
+        yroot = self.find(parent, y)
+        if rank[xroot] < rank[yroot]:
+            parent[xroot] = yroot
+        elif rank[xroot] > rank[yroot]:
+            parent[yroot] = xroot
+        else:
+            parent[yroot] = xroot
+            rank[xroot] += 1
+
+    def kruskal(self):
+        result = []
+        i, e = 0, 0
+        self.graph = sorted(self.graph, key=lambda item: item[2])
+        parent = [i for i in range(self.V)]
+        rank = [0] * self.V
+
+        while e < self.V - 1:
+            u, v, w = self.graph[i]
+            i += 1
+            x = self.find(parent, u)
+            y = self.find(parent, v)
+            if x != y:
+                e += 1
+                result.append([u, v, w])
+                self.apply_union(parent, rank, x, y)
+        for u, v, weight in result:
+            print(f"Edge: {u} - {v} ({weight})")
+
+# Example usage:
+g = Graph(5)
+g.add_edge(0, 1, 8)
+g.add_edge(0, 2, 5)
+g.add_edge(1, 2, 9)
+g.add_edge(1, 3, 11)
+g.add_edge(2, 3, 15)
+g.add_edge(2, 4, 10)
+g.add_edge(3, 4, 7)
+
+g.kruskal()
