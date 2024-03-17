13d265b42f817238bc37119d48fa85b1b4ef806c
Pranab Kumar Nahak <112896313+Pranab-987@users.noreply.github.com>
2024-03-18 01:20:04 +0530

    Add files via upload

---
+++ bellman17ford.py
@@ -0,0 +1,53 @@
+class Graph:
+  def __init__(self, vertices):
+    self.graph = {}
+    self.V = vertices
+
+  def add_edge(self, u, v, w):
+    if u not in self.graph:
+      self.graph[u] = {}
+    self.graph[u][v] = w
+
+  def print_graph(self):
+    print("{")
+    for i in self.graph:
+      print(f"  {i}: {self.graph[i]}")
+    print("}")
+
+  def bellman_ford(self, start):  
+    edge_list = []
+    for i in self.graph:
+      for j in self.graph[i]:
+        edge_list.append((i, j))
+    print(edge_list)
+    
+    path = {}
+    for i in self.graph:
+      path[i] = float('inf')
+    print(path)
+    path[start] = 0
+    
+    for i in range(self.V - 1):
+      for j in edge_list:
+        s = path[j[0]]
+        path[j[1]] = min(path[j[1]], s+self.graph[j[0]][j[1]])
+
+    for i in self.graph:
+      print(f"Minimum Distance of vertice {i} from {start} is {path[i]}")
+
+g = Graph(7)
+g.add_edge(1, 2, 6)
+g.add_edge(1, 3, 5)
+g.add_edge(1, 4, 5)
+g.add_edge(2, 5, -1)
+g.add_edge(3, 2, -2)
+g.add_edge(3, 5, 1)
+g.add_edge(4, 3, -2)
+g.add_edge(4, 6, -1)
+g.add_edge(5, 7, 3)
+g.add_edge(6, 7, 3)
+g.add_edge(7, 7, 0)
+
+g.print_graph()
+print()
+g.bellman_ford(1)
