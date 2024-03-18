cc955077ce7797f8b328ca254da86181dd2f4cc7
Biswajit Sahoo <160457169+Hypercoder09@users.noreply.github.com>
2024-03-18 09:28:01 +0530

    Add files via upload

---
+++ KNAPSACK.py
@@ -0,0 +1,21 @@
+def knapSack(W, wt, val, n):
+   if n == 0 or W == 0 :
+      return 0
+  
+   if (wt[n-1] > W):
+      return knapSack(W, wt, val, n-1)
+   
+   else:
+      return max(val[n-1] + knapSack(W-wt[n-1], wt, val, n-1),
+         knapSack(W, wt, val, n-1))
+   
+# data
+val = [10,5,15,7,6,18,3]
+wt = [2,3,5,7,1,4,1]
+W = 15
+n = len(val)
+print (knapSack(W, wt, val, n))
+    
+
+# M = [[0] * (15 + 1) for _ in range(7 + 1)]
+# print(M)
