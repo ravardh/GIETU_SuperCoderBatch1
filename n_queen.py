ae93afa334cae15ab24f959bcd2264824f10035b
Sushree Sasmita <143263248+sushreesasmitanayak@users.noreply.github.com>
2024-03-18 10:47:13 +0530

    Add files via upload

---
+++ n_queen.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def is_safe(board, row, col, n):
+    # Check if there is a queen in the same column
+    for i in range(row):
+        if board[i][col] == 1:
+            return False
+
+    # Check upper diagonal on left side
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    # Check upper diagonal on right side
+    for i, j in zip(range(row, -1, -1), range(col, n)):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+def solve_n_queens_util(board, row, n):
+    if row == n:
+        return True
+
+    for col in range(n):
+        if is_safe(board, row, col, n):
+            board[row][col] = 1
+            if solve_n_queens_util(board, row + 1, n):
+                return True
+            board[row][col] = 0
+
+    return False
+
+def solve_n_queens(n):
+    board = [[0] * n for _ in range(n)]
+    if not solve_n_queens_util(board, 0, n):
+        print("No solution exists.")
+        return
+    for row in board:
+        print(" ".join(map(str, row)))
+
+# Example usage:
+n = 4
+solve_n_queens(n)
+
+
+# In[ ]:
+
+
+
+
