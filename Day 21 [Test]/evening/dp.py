936cf0dc44f8da835695811aede4e61355db50d4
Ankit Choudhury <ankit.choudhury0608@gmail.com>
2024-03-21 09:45:01 +0530

    Day 22
    Completed all the questions

---
+++ Day 21 [Test]/evening/dp.py
@@ -0,0 +1,30 @@
+def find_max_profit(l, p):
+  n = len(l)
+  dp = [0] * (max(l)+1)
+  for i, j in zip(l, p):
+    # initialize the dp
+    dp[i] = j
+  # print(*dp)
+  for i in l:
+    for j in range(i):
+      dp[i] = max(dp[i], dp[j]+dp[i-j])
+  # print(*dp)
+  return dp[n]
+
+if __name__ == "__main__":
+  # l = list(map(int, input().strip().split()))
+  # p = list(map(int, input().strip().split()))
+  # print(find_max_profit(l, p))
+  testcases = {
+    "1": {
+      "length": [1, 2, 3, 4, 5, 6, 7, 8], 
+      "price": [1, 5, 8, 9, 10, 17, 17, 20]
+    },
+    "2": {
+      "length": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], 
+      "price": [3, 5, 8, 9, 10, 17, 17, 20, 22, 25, 30, 35]
+    }
+  }
+  inp = input("Choose Testcase[1/2]: ")
+  print(f"Testcase #{inp}:\nLength: {testcases[inp]["start"]}\nPrice: {testcases[inp]["price"]}")
+  print(find_max_profit(testcases[inp]["start"], testcases[inp]["price"]))
--- Day 21 [Test]/evening/room_schedule.py
+++ Day 21 [Test]/evening/room_schedule.py
@@ -1,36 +1,45 @@
-def min_meeting_rooms(schedule):
-  # Sort meetings based on end time
-  schedule.sort(key=lambda x: x[1])
+def min_rooms(schedule):
+  schedule.sort()
+  # print(*schedule)
 
   rooms = [schedule[0]]
 
   # behaves like a min heap O(n log n)
   def current_room(rooms):
+    # Sort timings based on end time
     rooms.sort(key=lambda x: x[1])
     return rooms[0]
 
   for i in range(1, len(schedule)):
+    # print(*rooms)
     room = current_room(rooms) # current room that can be alloted now
     time = schedule[i]
     if room[1] <= time[0]:  # If the room is available for the current meeting, update its end time.
       room[1] = time[1]
     else: # If the current meeting overlaps with existing meetings, allocate a new room
       rooms.append(time)
-
+  # print(*rooms)
   return len(rooms)
 
 def convert(time_str):
   hour, minute = map(int, time_str.split(':'))
   return hour * 60 + minute
 
-# Test cases
-s = ['10:00', '10:30', '11:00', '11:30']
-e = ['10:30', '11:00', '11:30', '12:00']
-print(min_meeting_rooms([[convert(i), convert(j)] for i, j in zip(s,e)]))  # Output: 2
-s = ['9:00', '9:30', '11:00', '12:00']
-e = ['10:15', '11:00', '12:15', '13:00']
-print(min_meeting_rooms([[convert(i), convert(j)] for i, j in zip(s,e)]))  # Output: 2
-
-s = ['9:00', '9:15', '9:30', '9:00', '10:15']
-e = ['10:00', '10:30', '10:45', '9:30', '11:30']
-print(min_meeting_rooms([[convert(i), convert(j)] for i, j in zip(s,e)]))  # Output: 3
+if __name__ == "__main__":
+  testcases = {
+    "1": {
+      "start": ['10:00', '10:30', '11:00', '11:30'],
+      "end": ['10:30', '11:00', '11:30', '12:00']
+    },
+    "2": {
+      "start": ['9:00', '9:30', '11:00', '12:00'],
+      "end": ['10:15', '11:00', '12:15', '13:00']
+    },
+    "3": {
+      "start": ['9:00', '9:15', '9:30', '9:00', '10:15'],
+      "end": ['10:00', '10:30', '10:45', '9:30', '11:30']
+    }
+  }
+  inp = input("Choose Testcase[1/2/3]: ")
+  print(f"Testcase #{inp}:\Start: {testcases[inp]["start"]}\End: {testcases[inp]["end"]}")
+  
