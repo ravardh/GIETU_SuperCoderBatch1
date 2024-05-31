b151f896d1009332c87df027ff42955df072abb2
Utkarshverma833 <160510395+Utkarshverma833@users.noreply.github.com>
2024-05-31 22:09:25 +0530

    Add files via upload

---
+++ Week 1 Q2.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    if (a>=1 and a<=1024) and (b>=1 and b<=1024):
+        print(a + b)
+        print(a - b)
+        print(a * b)
